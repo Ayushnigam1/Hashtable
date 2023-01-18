cdb94481417b1532e69dba84b5763d276b2c97ff
Ayushnigam1 <abhay27nigam02@gmail.com>
2023-01-18 20:02:32 +0530

    question and subsection

---
+++ src/pages/Subsection.tsx
@@ -0,0 +1,97 @@
+import Navbar from "components/Navbar";
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Grid } from "@mui/material";
+import Box from "@mui/material/Box";
+
+const Subsection = () => {
+  return (
+    <>
+      <Navbar />
+      <Box sx={{
+              display: "flex",
+              
+            }}>
+        <Box>
+          <Card sx={{ maxWidth: 300 ,margin:"50px 100px 0px 0px",height: 500}}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            </Box>
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1>Sub-section Name</h1>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "0px 0 50px 0px",
+            }}
+          >
+            <Card sx={{ maxWidth: 500 }}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "0px 0 50 0px",
+            }}
+          >
+            <Card sx={{ maxWidth: 500, height: 400 }}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Subsection;
