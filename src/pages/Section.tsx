8dc7c406618a41fbd7c03d74e0abc323cca1e81c
Ayushnigam1 <abhay27nigam02@gmail.com>
2023-01-20 09:03:10 +0530

    local

--- src/pages/Section.tsx
+++ src/pages/Section.tsx
@@ -1,4 +1,4 @@
-import Navbar from 'components/Navbar'
+import Navbar from '../components/Navbar'
 import React from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
