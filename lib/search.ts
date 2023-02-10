99ccd0174a4e07b54c47f2de7a42fd8b2772a625
Ashwin <ashwin200113@gmail.com>
2023-02-10 19:22:16 +0530

    search logic

--- lib/search.ts
+++ lib/search.ts
@@ -1,9 +1,10 @@
 import { Question } from "lib/question"
 
-export function search(searchString: string, tags: string[], difficulty: string[], questions: Question[]): Question[] {
+export function search(searchString: string, tags: string[], difficulty: string | null, topic: string | null, questions: Question[]): Question[] {
     const question = questions.filter(question => (
             (question.tags.some((tag: string) => tags.includes(tag)) || tags.length == 0) &&
-            (difficulty.includes(question.difficulty) || difficulty.length == 0) &&
+            (!difficulty || question.difficulty == difficulty) &&
+            (!topic || question.topic.includes(topic)) &&
             question.id.toLowerCase().includes(searchString.toLowerCase())
         )
     )
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -6,13 +6,26 @@ import Search from "@/components/Search"
 import { Hero } from "@/components/Hero"
 import Router from "next/router"
 import Tag from "@/components/Tag"
-import {Select} from "@mantine/core"
 import { search } from "lib/search"
-
-const tg = []
+import { Button, Select } from "@mantine/core"
 
 const tp = [
+    "Array",
+    "Linked List",
+    "Stack",
+    "Queue",
+    "Graph",
+    "Trees"
+]
 
+const tg = [
+    "Recursion",
+    "Two pointer",
+    "Merge",
+    "DP",
+    "Sorting",
+    "Linear Search",
+    "Binary search",
 ]
 
 const diff = [
@@ -50,18 +63,13 @@ const Searchpage = ({ question }: { question: any[] }) => {
     const [tags, setTags] = useState<string[]>([]);
     const [ques, setQuestions] = useState<Question[]>([]);
 
-    const handleAdd = (tag: string) => {
-        setTags([...tags, tag]);
-    };
-
     const handleRemove = (tag: string) => {
         setTags(tags.filter((t) => t !== tag));
-        Router.back();
     };
 
     useEffect(() => {
-        setQuestions(search(searchdata, [], [], question))
-    }, [question, searchdata])
+        setQuestions(search(searchdata, tags, difficulty, topics, question))
+    }, [question, searchdata, tags, difficulty, topics])
 
     return (
         <>
@@ -71,37 +79,41 @@ const Searchpage = ({ question }: { question: any[] }) => {
                 <Search onEnter={(searchTerm) => Router.push(`/search?keyword=${searchTerm.toLowerCase()}`)} />
             </Hero>
             <section className="xl:max-w-[120ch] mx-4 mt-4 xl:mx-auto">
-            <div className="flex justify-center flex-col sm:flex-row py-4">
-                <div className="xl:w-80 m-3">
-                    <select className="w-full px-5 py-1.5 font-semibold prose dark:prose-invert border border-gray-600 rounded transition ease-in-out focus:prose focus:outline-none" aria-label="Default select example" onSelect={(e) => console.log(e.currentTarget.item)}>
-                        <option selected disabled>Tags</option>
-                        <option value="recursion">Recursion</option>
-                        <option value="2">Two pointer</option>
-                        <option value="3">Merge</option>
-                        <option value="4">DP</option>
-                        <option value="5">Sorting</option>
-                        <option value="6">Linear Search</option>
-                        <option value="7">Binary search</option>
-                    </select>
+                <div className="flex justify-between flex-col sm:flex-row py-4 gap-4 sm:justify-right">
+                    <Select
+                        className="flex-grow"
+                        placeholder="Difficulty"
+                        onChange={(e) => { setDifficulty(e) }}
+                        data={diff.map(d => d.level)}
+                    />
+                    <div className="flex-grow">
+                        <Select
+                            placeholder="Tags"
+                            onChange={(e) => {
+                                if (e != undefined) setTags([...tags, e])
+                            }}
+                            data={tg}
+                        />
+                    </div>
+                    <Select
+                        className="flex-grow"
+                        placeholder="Topics"
+                        onChange={(e) => setTopics(e)}
+                        data={tp}
+                    />
+                    <Button className="rounded-full" onClick={(e) => {
+                        setTopics(null)
+                        setTags([])
+                        setDifficulty(null)
+                    }}>Reset Filters</Button>
+                </div>
+                <div className="flex gap-3">
                     {tags.map((tag) => (
                         <Tag key={tag} tag={tag} onRemove={handleRemove} />
                     ))}
                 </div>
-                <div className="m-3 xl:w-80 ">
-                    <select className="w-full px-5 py-1.5 font-semibold prose dark:prose-invert border border-gray-600 rounded transition ease-in-out focus:prose focus:outline-none ">
-                        <option selected disabled>Topics</option>
-                        <option value="array">Arrays</option>
-                        <option value="linked list">Linked List</option>
-                        <option value="stack">Stack</option>
-                        <option value="queue">Queue</option>
-                        <option value="graph">Graph</option>
-                        <option value="tree">Trees</option>
-                    </select>
-                </div>
-            </div>
                 <div className="flex-grow">
-                    <h3 className="font-bold text-2xl">Questions</h3>
-                    <table className="table-auto w-full ">
+                    <table className="table-auto w-full">
                         <thead>
                             <tr>
                                 {["#", "Name", "Difficulty"].map((heading, index) => (
@@ -118,24 +130,24 @@ const Searchpage = ({ question }: { question: any[] }) => {
                             {ques.length > 0 ? (
                                 <>
                                     {ques.map((question: any, index: number) => {
-                                            return (
-                                                <tr
-                                                    key={index}
-                                                    className="transition ease-in-out hover:scale-105 hover:bg-slate-200 dark:hover:bg-gray-700">
-                                                    <td className="border-b border-gray-300 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                        {index + 1}
-                                                    </td>
-                                                    <td className="border-b border-gray-300 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 capitalize">
-                                                        <a href={`./questions/${question.id}`}>
-                                                            {question.id.replace(/_/g, " ")}
-                                                        </a>
-                                                    </td>
-                                                    <td className="border-b  border-gray-300 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                        {question.difficulty}
-                                                    </td>
-                                                </tr>
-                                            );
-                                        })}
+                                        return (
+                                            <tr
+                                                key={index}
+                                                className="transition ease-in-out hover:scale-105 hover:bg-slate-200 dark:hover:bg-gray-700">
+                                                <td className="border-b border-gray-300 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    {index + 1}
+                                                </td>
+                                                <td className="border-b border-gray-300 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 capitalize">
+                                                    <a href={`./questions/${question.id}`}>
+                                                        {question.id.replace(/_/g, " ")}
+                                                    </a>
+                                                </td>
+                                                <td className="border-b  border-gray-300 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    {question.difficulty}
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
                                 </>
                             ) : (
                                 <></>
